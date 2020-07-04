d26de2dc06cd04d8416c7133de721b4cdfc4c544
revess <bjmaat@gmail.com>
2020-07-04 20:47:56 +0200

    subfinal version for website

--- javascript/fadeAnimationMainpage.js
+++ javascript/fadeAnimationMainpage.js
@@ -1,36 +1,36 @@
-$(window).scroll(function() {
-  //For the navbar fade
-  if ($(window).width() > 991) {
-    if (
-      $(window).scrollTop() <
-      $("#homeBox").position().top + $("#homeBox").outerHeight()
-    ) {
-      $("#navBarTop").addClass("navTrans");
-    }
-    if (
-      $(window).scrollTop() >=
-      $("#homeBox").position().top + $("#homeBox").outerHeight()
-    ) {
-      $("#navBarTop").removeClass("navTrans");
-    }
-  }
-  if (
-    $(window).scrollTop() +
-      $(window).outerHeight() -
-      $("#downArrow").outerHeight() >
-    $("#About").position().top
-  ) {
-    $("#homeArrow").css("transition", "all 0.2s ease-in-out");
-    $("#homeArrow").css("color", "rgba(0,0,0,0");
-    $("#downArrow").animate
-  }
-  if (
-    $(window).scrollTop() +
-      $(window).outerHeight() -
-      $("#downArrow").outerHeight() <
-    $("#About").position().top
-  ) {
-    $("#homeArrow").css("transition", "all 0.2s ease-in-out");
-    $("#homeArrow").css("color", "rgb(0,0,0)");
-  }
-});
+// $(window).scroll(function() {
+//   //For the navbar fade
+//   if ($(window).width() > 991) {
+//     if (
+//       $(window).scrollTop() <
+//       $("#homeBox").position().top + $("#homeBox").outerHeight()
+//     ) {
+//       $("#navBarTop").addClass("navTrans");
+//     }
+//     if (
+//       $(window).scrollTop() >=
+//       $("#homeBox").position().top + $("#homeBox").outerHeight()
+//     ) {
+//       $("#navBarTop").removeClass("navTrans");
+//     }
+//   }
+//   if (
+//     $(window).scrollTop() +
+//       $(window).outerHeight() -
+//       $("#downArrow").outerHeight() >
+//     $("#About").position().top
+//   ) {
+//     $("#homeArrow").css("transition", "all 0.2s ease-in-out");
+//     $("#homeArrow").css("color", "rgba(0,0,0,0");
+//     $("#downArrow").animate
+//   }
+//   if (
+//     $(window).scrollTop() +
+//       $(window).outerHeight() -
+//       $("#downArrow").outerHeight() <
+//     $("#About").position().top
+//   ) {
+//     $("#homeArrow").css("transition", "all 0.2s ease-in-out");
+//     $("#homeArrow").css("color", "rgb(0,0,0)");
+//   }
+// });
--- javascript/scrollAnimation.js
+++ javascript/scrollAnimation.js
@@ -1,46 +1,46 @@
-var navLinks = [
-  "#homeLink",
-  "#aboutLink",
-  "servicesLink",
-  "#contactLink"
-];
-//Scroll and move highlight
-$(window).scroll(function() {
-  let navHeight = $("#topNav").outerHeight();
-  if (
-    $(window).scrollTop() + navHeight >= 0 &&
-    $(window).scrollTop() + navHeight <= $("#Home").outerHeight()
-  ) {
-    $("#homeLink").attr("style", "color: rgb(0,0,0) !important");
-    $.each(navLinks, function(index, value) {
-      if (value !== "#homeLink") {
-        $(value).attr("style", "color: rgba(0,0,0,0.5) !important");
-      }
-    });
-  }
-  //Uncomment for scroll animations
-  if (
-    $(window).scrollTop() + navHeight >= $("#About").position().top &&
-    $(window).scrollTop() + navHeight <
-      $("#About").position().top + $("#About").outerHeight()
-  ) {
-    $("#aboutLink").attr("style", "color: rgb(0,0,0) !important");
-    $.each(navLinks, function(index, value) {
-      if (value !== "#aboutLink") {
-        $(value).attr("style", "color: rgba(0,0,0,0.5) !important");
-      }
-    });
-  }
-  // if (
-  //   $(window).scrollTop() + navHeight >= $("#Contact").position().top ||
-  //   $(window).scrollTop() + navHeight + $(window).outerHeight() >=
-  //     $("footer").position().top
-  // ) {
-  //   $("#contactLink").attr("style", "color: rgb(0,0,0) !important");
-  //   $.each(navLinks, function(index, value) {
-  //     if (value !== "#contactLink") {
-  //       $(value).attr("style", "color: rgba(0,0,0,0.5) !important");
-  //     }
-  //   });
-  // }
-});
+// var navLinks = [
+//   "#homeLink",
+//   "#aboutLink",
+//   "servicesLink",
+//   "#contactLink"
+// ];
+// //Scroll and move highlight
+// $(window).scroll(function() {
+//   let navHeight = $("#topNav").outerHeight();
+//   if (
+//     $(window).scrollTop() + navHeight >= 0 &&
+//     $(window).scrollTop() + navHeight <= $("#Home").outerHeight()
+//   ) {
+//     $("#homeLink").attr("style", "color: rgb(0,0,0) !important");
+//     $.each(navLinks, function(index, value) {
+//       if (value !== "#homeLink") {
+//         $(value).attr("style", "color: rgba(0,0,0,0.5) !important");
+//       }
+//     });
+//   }
+//   //Uncomment for scroll animations
+//   if (
+//     $(window).scrollTop() + navHeight >= $("#About").position().top &&
+//     $(window).scrollTop() + navHeight <
+//       $("#About").position().top + $("#About").outerHeight()
+//   ) {
+//     $("#aboutLink").attr("style", "color: rgb(0,0,0) !important");
+//     $.each(navLinks, function(index, value) {
+//       if (value !== "#aboutLink") {
+//         $(value).attr("style", "color: rgba(0,0,0,0.5) !important");
+//       }
+//     });
+//   }
+//   // if (
+//   //   $(window).scrollTop() + navHeight >= $("#Contact").position().top ||
+//   //   $(window).scrollTop() + navHeight + $(window).outerHeight() >=
+//   //     $("footer").position().top
+//   // ) {
+//   //   $("#contactLink").attr("style", "color: rgb(0,0,0) !important");
+//   //   $.each(navLinks, function(index, value) {
+//   //     if (value !== "#contactLink") {
+//   //       $(value).attr("style", "color: rgba(0,0,0,0.5) !important");
+//   //     }
+//   //   });
+//   // }
+// });
